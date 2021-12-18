7198c1a80b5104d33267fbd2a933880da1d8a384
ElaheSmailpour <eli_es87@yahoo.com>
2021-12-18 16:38:23 +0100

    drawer

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route,Switch } from 'react-router-dom';
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import  DrawerMenu from "./pages/DrawerMenu"
+function App() {
+  return (
+   
+      <BrowserRouter>
+     
+        <div className="app-content">
+         
+          <DrawerMenu/>
+          <Switch>
+          <Route path={"/"} component={Home}></Route>
+          <Route path={"/contact"} component={Contact}></Route>
+          <Route path={"/about"} component={About}></Route>
+        
+          </Switch>
+        </div>
+      </BrowserRouter>
+
+   
+  );
+}
+
+export default App;
